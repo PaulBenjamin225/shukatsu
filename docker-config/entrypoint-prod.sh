38dded5f15eade3de223ec3407aa4fb51c1f89f5
AHOUTOU Paul-Benjamin <paulbenjaminahoutou@gmail.com>
2025-10-01 14:16:04 +0000

    Final

--- docker-config/entrypoint-prod.sh
+++ docker-config/entrypoint-prod.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# On se place dans le bon dossier
 cd /var/www/html
 
-# On utilise le chemin absolu vers PHP pour être sûr
-echo "--- Lancement des migrations ---"
-/usr/local/bin/php artisan migrate --force
+# On s'assure que les caches sont à jour avec les variables d'environnement
+php artisan config:cache
+php artisan route:cache
+
+# On lance les migrations
+php artisan migrate --force
 
 # On lance Apache
-echo "--- Démarrage du serveur Apache ---"
 exec apache2-foreground
