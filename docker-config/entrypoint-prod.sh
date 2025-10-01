d29c1ff36be531488395627fde7d735c1e913084
AHOUTOU Paul-Benjamin <paulbenjaminahoutou@gmail.com>
2025-10-01 14:51:26 +0000

    Final

--- docker-config/entrypoint-prod.sh
+++ docker-config/entrypoint-prod.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-cd /var/www/html
+# On se place dans le répertoire de l'application
+cd /app
 
-# On s'assure que les caches sont à jour avec les variables d'environnement
-php artisan config:cache
-php artisan route:cache
+# On utilise le chemin absolu vers PHP pour une robustesse maximale
+echo "--- Lancement des migrations ---"
+/usr/local/bin/php artisan migrate --force
 
-# On lance les migrations
-php artisan migrate --force
-
-# On lance Apache
-exec apache2-foreground
+# On lance le serveur Octane. 'exec' est crucial.
+echo "--- Démarrage du serveur Octane sur le port 10000 ---"
+exec /usr/local/bin/php artisan octane:start --host=0.0.0.0 --port=10000
